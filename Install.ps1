347dfa221251bf8eea7b41fcd4ac3bfcfb5f680e
tunisiano187 <info@bowlman.org>
2015-12-27 11:44:06 +0100

    Download Git portable and 7z app

--- Install.ps1
+++ Install.ps1
@@ -1,7 +1,17 @@
 Write-Host -ForegroundColor 'GREEN' Download Git
+# Functions
+function Download-File {
+	param (
+		[string]$url,
+		[string]$file
+	)
+	Write-Host "Downloading $url to $file"
+	$webclient = New-Object System.Net.WebClient
+	$webclient.DownloadFile($url,$file)
+}
 # Configuration
 $unzipMethod = '7zip'
-$7ZURL= 'https://chocolatey.org/7za.exe'
+$7zURL= 'https://github.com/tunisiano187/WindowsFirstBoot/raw/master/Apps/7za.exe'
 
 $installDir = Join-Path $env:temp 'winfirstboot'
 New-Item $installDir -type directory
@@ -12,15 +22,15 @@ $url64 = 'https://github.com/git-for-windows/git/releases/download/v2.6.4.window
 # Check OS Architecture 
 $OSArch =Get-WmiObject Win32_OperatingSystem  | select OSArchitecture
 
-# Downloading file
-$webclient = New-Object System.Net.WebClient
+# Downloading Git file
 if ($OSArch.OSArchitecture -eq '64 bits') 
 {
   $url = $url64
 }
-$file = "$installDir\PortableGit.7z.exe"
-$webclient.DownloadFile($url,$file)
+Write-Host "Download Git Portable 7z"
+$GitExe = Join-Path $installDir 'PortableGit.7z.exe'
+Download-File $url "$GitExe"
 
-Write-Output "Download 7Zip commandline tool"
-  $7zaExe = Join-Path $tempDir '7za.exe'
-  Download-File $7zURL "$7zaExe"
+Write-Host "Download 7Zip commandline tool"
+$7zaExe = Join-Path $installDir '7za.exe'
+Download-File $7zURL "$7zaExe"
