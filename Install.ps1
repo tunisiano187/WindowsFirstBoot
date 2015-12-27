c60e770c98b6de520523b2387e0a208eabe8b684
tunisiano187 <info@bowlman.org>
2015-12-27 17:01:46 +0100

    Try BitsTransfer

--- Install.ps1
+++ Install.ps1
@@ -6,39 +6,26 @@ function Download-File {
 		[string]$file
 	)
 	Write-Host "Downloading $url to $file"
-	$webclient = New-Object System.Net.WebClient
-	$webclient.DownloadFile($url,$file)
+	#$webclient = New-Object System.Net.WebClient
+	#$webclient.DownloadFile($url,$file)
+	Import-Module BitsTransfer
+	Start-BitsTransfer $url $file
 }
 # Configuration
-$unzipMethod = '7zip'
-$7zURL= 'https://github.com/tunisiano187/WindowsFirstBoot/raw/master/Apps/7za.exe'
+$RepoURL= 'https://github.com/tunisiano187/WindowsFirstBoot/raw/master/'
+$BGInfoURL='https://download.sysinternals.com/files/BGInfo.zip'
 
 $installDir = Join-Path $env:temp 'winfirstboot'
 New-Item $installDir -type directory
 
-$url = 'https://github.com/tunisiano187/WindowsFirstBoot/raw/master/Apps/PortableGit32.zip'
-$url64 = 'https://github.com/tunisiano187/WindowsFirstBoot/raw/master/Apps/PortableGit64.zip'
-
 # Check OS Architecture 
 $OSArch =Get-WmiObject Win32_OperatingSystem  | select OSArchitecture
-
-# Downloading Git file
 if ($OSArch.OSArchitecture -eq '64 bits') 
 {
   $url = $url64
 }
-Write-Host "Download Git Portable zip"
-$GitZip = Join-Path $installDir 'PortableGit.zip'
-Download-File $url "$GitZip"
-
-Write-Host "Download 7Zip commandline tool"
-$7zaExe = Join-Path $installDir '7za.exe'
-Download-File $7zURL "$7zaExe"
-
-Write-Host -ForegroundColor Green Extract Files
-Start-Process "$7zaExe" -ArgumentList "x -o`"$installDir`" -y `"$GitZip`"" -Wait -NoNewWindow
 
 cd $installDir
-PortableGit\cmd\git.exe clone https://github.com/tunisiano187/WindowsFirstBoot.git
-
+$BGInfoZip=Join-Path $installDir 'BGInfo.zip'
+Download-File $BGInfoURL $BGInfoZip
 
