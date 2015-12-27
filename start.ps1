d24a8e1971bd623e67900e4085a8dd8f4778e61d
tunisiano187 <info@bowlman.org>
2015-12-27 21:14:14 +0100

    Change in remove folder

--- start.ps1
+++ start.ps1
@@ -11,7 +11,7 @@ function Download-File {
 }
 # Configuration
 $installDir = Join-Path $env:temp 'winfirstboot'
-$installDirFiles = Join-Path $installDir "\*"
+$installDirFiles = Join-Path "$installDir\*"
 Remove-Item $installDirFiles -recurse
 $RepoURL= 'https://github.com/tunisiano187/WindowsFirstBoot/raw/master/'
 $InstallFileURL = $RepoURL + 'Install.ps1'
